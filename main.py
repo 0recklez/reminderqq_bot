da46375d3cb7c0913a56bd759d2df922a6b4dda0
женек <evgenkasatik.com@gmail.com>
2025-04-09 23:10:44 +0300

    fix time notification

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ async def process_add_task_time(message: Message, state: FSMContext):
     data = await state.get_data()
     current_time = datetime.now() + timedelta(hours=3)
     try:
-        task_time = datetime.strptime(message.text, "%d.%m.%Y %H:%M") + timedelta(hours=3)
+        task_time = datetime.strptime(message.text, "%d.%m.%Y %H:%M")
     except ValueError:
         await message.answer(
             "❌ Неверный формат времени!\n"
@@ -102,7 +102,7 @@ async def process_add_task_time(message: Message, state: FSMContext):
     }
     scheduler.add_job(
         send_reminder,
-        trigger=DateTrigger(task_time),
+        trigger=DateTrigger(task_time + timedelta(hours=3)),
         args=(user_id, new_task['text']),
         id=f"reminder_{user_id}_{new_task['id']}"
     )
